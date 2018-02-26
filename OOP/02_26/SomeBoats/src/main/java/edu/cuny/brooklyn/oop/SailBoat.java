0b4cc0858193a552860468ca1e66a8b48199ce93
Hui Chen <chen@sci.brooklyn.cuny.edu>
2018-02-25 21:05:13 -0500

    updated boat and rowboat example with copyright info

--- OOP/02_26/SomeBoats/src/main/java/edu/cuny/brooklyn/oop/SailBoat.java
+++ OOP/02_26/SomeBoats/src/main/java/edu/cuny/brooklyn/oop/SailBoat.java
@@ -1,3 +1,8 @@
+/*
+ * This program is in page 196, chapter 7 from 
+ * Sierra, Kathy. 2005. Head First Java (2nd ed.). O'Reilly Media, Inc
+ */
+
 package edu.cuny.brooklyn.oop;
 
 class Sailboat extends Boat {
