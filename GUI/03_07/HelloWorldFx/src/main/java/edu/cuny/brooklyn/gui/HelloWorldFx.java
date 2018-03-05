10c1e8268e6749c774e466ee02c54ee88b6383e5
Hui Chen <chen@sci.brooklyn.cuny.edu>
2018-03-05 18:42:46 -0500

    helloworldfx

---
+++ GUI/03_07/HelloWorldFx/src/main/java/edu/cuny/brooklyn/gui/HelloWorldFx.java
@@ -0,0 +1,42 @@
+package edu.cuny.brooklyn.gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorldFx extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		Button btn = new Button();
+		btn.setText("Say 'Hello World' in the Console");
+		
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+			}
+		});
+		
+		// EventHandler is a functional interface, so we can,
+//		btn.setOnAction(event -> System.out.println("Hello World!"));
+
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+
+		Scene scene = new Scene(root, 300, 250);
+
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ GUI/03_07/HelloWorldFx/src/test/java/edu/cuny/brooklyn/cisc3120/HelloWorldFx/AppTest.java
@@ -0,0 +1,17 @@
+package edu.cuny.brooklyn.cisc3120.HelloWorldFx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
